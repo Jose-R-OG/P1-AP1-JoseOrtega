ef1cef0a63220f6c9ad039c26ae089b724223d87
José Rafael Ortega González <jose_ortega2@ucne.edu.do>
2025-09-22 19:20:09 -0400

    Creacion del modelo

---
+++ P1-AP1-JoseOrtega/Models/Registro.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P1_AP1_JoseOrtega.Models;
+
+public class Registro
+{
+    [Key]
+    public int Id { get; set; }
+
+}
